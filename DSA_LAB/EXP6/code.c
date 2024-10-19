3165a51c8dd885247b2cebb8d78768a84e7ddbd9
sanketp1708 <2023.sanket.patil@ves.ac.in>
2024-10-19 16:32:30 +0530

    code.c

--- DSA_LAB/EXP6/code.c
+++ DSA_LAB/EXP6/code.c
@@ -1,61 +1,151 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
+CODE
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
     int data;
-    struct Node* next;
-};
-
-struct Node* top = NULL;
-
-void push(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = top;
-    top = newNode;
+    struct node* next;
+}node;
+void push(struct node** top,int data);
+void display(struct node* top);
+int isempty(struct node* top);
+int peek(struct node* top);
+void push(struct node** top,int data){
+    struct node* newnode=(struct node*)malloc(sizeof(node));
+    if (!newnode){
+        printf("Stack overflow\n");
+        return;
+    }
+    newnode->data=data;
+    newnode->next=*top;
+    *top=newnode;
 }
-
-int pop() {
-    if (top == NULL) {
-        printf("Stack Underflow\n");
+int isempty(struct node * top){
+    return top==NULL;}
+int pop (struct node ** top){
+    if(isempty(*top)){
+        printf("Stack underflow\n");
         return -1;
     }
-    int value = top->data;
-    struct Node* temp = top;
-    top = top->next;
-    free(temp);
-    return value;
+    struct node* temp=*top;
+    *top=(*top)->next;
+    int popped=temp->data;
+    free (temp);
+    return popped;
 }
-
-int peek() {
-    if (top == NULL) {
-        printf("Stack is empty\n");
+int peek(struct node* top){
+    if(isempty(top)){
+        printf("stack is empty\n");
         return -1;
     }
     return top->data;
 }
-
-int isEmpty() {
-    return top == NULL;
+void display(struct node* top){  
+    struct node* temp=top;
+    while(temp){
+    printf("%d->",temp->data);
+    temp=temp->next;}
+    printf("NULL\n");
 }
-
-void display() {
-    struct Node* temp = top;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
+int main(){
+    struct node* stack= NULL;
+    int choice,value;
+    do{
+        printf("Menu:\n");
+        printf("1. push\n");
+        printf("2. pop\n");
+        printf("3. peek\n");
+        printf("4. display\n");
+        printf("5. exit\n");
+        printf("enter your choice : ");
+        scanf("%d",&choice);
+    switch(choice){
+        case 1:
+         printf("Enter the value to push: ");
+         scanf("%d",&value);
+         push(&stack,value);
+         break;
+        case 2:
+         value=pop(&stack);
+         if(value!=-1){
+            printf("Popped value: %d\n",value);
+            break;
+        }
+        case 3:
+         value=peek(stack);
+         if (value!=-1){
+             printf("Top value : %d\n",value);
+         }
+         break;
+        case 4:
+         display(stack);
+         break;
+        case 5:
+         printf("Exiting\n");
+         break;
+        default:
+         printf("invalid choice, try again\n");
     }
-    printf("\n");
-}
-
-int main() {
-    push(10);
-    push(20);
-    push(30);
-    display();
-    printf("Top element is %d\n", peek());
-    printf("Popped element is %d\n", pop());
-    display();
-    printf("Stack is empty: %s\n", isEmpty() ? "Yes" : "No");
+    }while (choice!=5);
     return 0;
 }
+
+OUTPUT
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 1
+Enter the value to push: 10
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 1
+Enter the value to push: 20
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 1
+Enter the value to push: 30
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 2
+Popped value: 30
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 3
+Top value : 20
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 4
+20->10->NULL
+Menu:
+1. push
+2. pop
+3. peek
+4. display
+5. exit
+enter your choice : 5
+Exiting
+
+
+=== Code Execution Successful ===
